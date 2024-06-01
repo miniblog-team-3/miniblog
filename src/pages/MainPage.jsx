2d3177a234adbbb35b8c89c4108f68302412ecbd
songjungah <1104wjddk@naver.com>
2024-06-01 13:00:44 +0900

    240601 nav, location add

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,11 +4,14 @@ import "./MainPage.css";
 import React from "react";
 import { minus, plus, reset, sum } from "../redux/userRedux";
 import PostListPage from "./PostListPage";
+import { useLocation } from "react-router-dom";
 
 export default function MainPage() {
   const reduxSelector = useSelector((state) => state);
 
   const dispatch = useDispatch();
+  const location = useLocation();
+  console.log('location : ', location);
   return (
     <div className="main-container">
       <h1>메인페이지 입니다.</h1>
